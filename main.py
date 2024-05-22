9a3b2f3874b43573b2ae679eb4ca6820341c59d4
Brandon <135168251+MrJiagge@users.noreply.github.com>
2024-05-22 12:30:17 +0100

    created global variables and constants

--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 from random import randint, choice
 from time import sleep as wait
 from json import load, dump
+
+#----------Global Variables----------
+player_1 = {
+    "name": input("👋 What is your name player 1?: "),
+    "cards": list[tuple[int, str]]
+}
+player_2 = {
+    "name": input("👋 What is your name player 2?: "),
+    "cards": list[tuple[int, str]]
+}
+deck_of_cards = []
+authorized_players = []
+colors = ["yellow", "red", "black"]
+
+#----------Constants----------
+NUMBER_OF_CARDS_IN_DECK = 30

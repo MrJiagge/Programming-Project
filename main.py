3815c725696c3b7a5214109f7466ff6ed89a465b
Brandon Jiagge <bejiagge@gmail.com>
2024-06-02 20:34:15 +0100

    winners can be calculated

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ player = dict[str, list[tuple[int, str]]]
 card = tuple[int, str]
 deck = list[card]
 
-
 #----------Global Variables----------
 player_1 = {
     "name": input("👋 What is your name player 1?: "),
@@ -51,4 +50,18 @@ def get_card_from_deck(deck, player) -> None:
     player["cards"].append(removed_card)
 
 def calculate_winner(player1, player2) -> player:
-    pass
+    # card from deck was APPENDED -> use -1 to get last element
+    # cards are a tuple[int, str] -> use 0 to get 1st element which is int(card number)
+    player1_card_number = player_1["cards"][-1][0]
+    player2_card_color = player_2["cards"][-1][1]
+    
+    # use 1 to get 2nd element which is str(card color)
+    player1_card_color = player_1["cards"][-1][1]
+    player2_card_number = player_2["cards"][-1][0]
+    
+    if player1_card_color != player2_card_color:
+        # if statement contains all possible ways player2 can win otherwise player1 wins
+        return player2 if (player1_card_color == "red" and player2_card_color == "yellow") or (player1_card_color == "yellow" and player2_card_color == "black") else player1
+    # return player who's card has a greater number if colors are equal
+    return player2 if player2_card_number > player1_card_number else player1
+    

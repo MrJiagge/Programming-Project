038acc502c345f5c99ded6f7804886239bb90468
Brandon Jiagge <bejiagge@gmail.com>
2024-06-02 19:51:05 +0100

    players can get a card from the deck + new type aliases

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ from random import randint, choice
 from time import sleep as wait
 from json import load, dump
 
+#----------Type Aliases----------
+player = dict[str, list[tuple[int, str]]]
+card = tuple[int, str]
+deck = list[card]
+
+
 #----------Global Variables----------
 player_1 = {
     "name": input("👋 What is your name player 1?: "),
-    "cards": list[tuple[int, str]]
+    "cards": deck
 }
 player_2 = {
     "name": input("👋 What is your name player 2?: "),
-    "cards": list[tuple[int, str]]
+    "cards": deck
 }
 deck_of_cards = []
 authorized_players = []
@@ -27,7 +33,7 @@ def load_authorized_players(file_path: str) -> list[str]:
 def check_players(player1_name, player2_name, authorized_players) -> bool:
     return player1_name.upper() and player2_name.upper() in authorized_players
 
-def create_deck(deck, number_of_cards) -> list[tuple[int, str]]:
+def create_deck(deck, number_of_cards) -> deck:
     existing_cards = set()
     
     while len(deck) < number_of_cards:
@@ -39,3 +45,10 @@ def create_deck(deck, number_of_cards) -> list[tuple[int, str]]:
             existing_cards.add(new_card)
             deck.append(new_card)
     return deck
+
+def get_card_from_deck(deck, player) -> None:
+    removed_card = deck.pop(0)
+    player["cards"].append(removed_card)
+
+def calculate_winner(player1, player2) -> player:
+    pass
